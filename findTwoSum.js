843d459e09e3b0ca85d8f414e37f3b9c8ce027b6
umi hui <umihui@umis-MacBook-Pro.local>
2018-04-18 18:00:20 -0700

    implement findTwoSum with O(n)

--- findTwoSum.js
+++ findTwoSum.js
@@ -4,26 +4,21 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
  */
 // input arr of num, num
 // output boolean
-// time O(n^2)
-// space O(1)
+// time O(n)
+// space O(2)?
 function findTwoSum(arr, k) {
   // check if num is less than k
   // call isEqual
-  while (arr[0] && arr.length > 1) {
-    if (arr[0] < k) {
-      for (let j = 1; j < arr.length; j++) {
-        if (isEqual(arr[0], arr[j], k)) {
-          return true;
-        }
-      }
+  var cache = {};
+  for (let num of arr) {
+    if (cache[num]) {
+      return true;
+    } else {
+      var rest = k - num;
+      cache[rest] = true;
     }
-    arr.shift();
   }
   return false;
 }
 
-function isEqual(num1, num2, k) {
-  return num1 + num2 === k;
-}
-
-console.log(findTwoSum([10, 14, 3, 6], 17));
+console.log(findTwoSum([10, 13, 3, 7], 17));
